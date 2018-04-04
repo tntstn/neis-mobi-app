c8b6e306037a11cb4124d07a1ba3f1eefad2c7e1
Tan TASTAN (NETAS External) <ttastan@netas.com.tr>
2018-04-04 13:48:57 +0300

    this is first commit

---
+++ src/main/java/mobi/neis/NeisMobiAppApplication.java
@@ -0,0 +1,12 @@
+package mobi.neis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeisMobiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeisMobiAppApplication.class, args);
+	}
+}
